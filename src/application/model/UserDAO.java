18a528aad8146303001399494158ff2a408d6218
Terkea <terkeabt@gmail.com>
2019-05-14 01:57:58 +0100

    other bugs

--- src/application/model/UserDAO.java
+++ src/application/model/UserDAO.java
@@ -224,13 +224,14 @@ public class UserDAO {
     public static ObservableList<User> getAllCustomers () throws SQLException, ClassNotFoundException{
         String selectStmt = "SELECT *\n" +
                 "FROM user\n" +
-                "WHERE (`corporate_organisation_name` = \"null\" OR `corporate_organisation_name` = \"NULL\" OR `corporate_organisation_name` = \"\" OR `corporate_organisation_name` = null)\n" +
+                "WHERE (`corporate_organisation_name` = \"null\" OR `corporate_organisation_name` = \"NULL\" OR `corporate_organisation_name` = \"\" " +
+                "OR `corporate_organisation_name` = null)\n" +
                 "AND\n" +
                 "(`events_organiser_name` = \"null\" OR `events_organiser_name` = \"NULL\" OR `events_organiser_name` = \"\" OR `events_organiser_name` = null)\n" +
                 "AND\n" +
                 "is_admin = false\n" +
                 "AND\n" +
-                "is_admin = false;";
+                "is_agent = false;";
         try {
             ResultSet rsUsers = DB.dbExecuteQuery(selectStmt);
             ObservableList<User> userList = getUserList(rsUsers);
